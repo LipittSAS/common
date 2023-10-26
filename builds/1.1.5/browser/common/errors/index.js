cc34d2ecd3092327f4933712838b54c5c85aecf6
Loïc Combis <combis.loic@gmail.com>
2023-10-27 01:34:51 +0200

    Build v1.1.5

---
+++ builds/1.1.5/browser/common/errors/index.js
@@ -0,0 +1,2 @@
+export * from './lib/error-codes.enum';
+//# sourceMappingURL=index.js.map
---
+++ builds/1.1.5/browser/core/media/lib/media-step-type.enum.js
@@ -0,0 +1,15 @@
+export var MediaStepType;
+(function (MediaStepType) {
+    MediaStepType["splitAudioAndVideo"] = "split_audio_and_video";
+    MediaStepType["dissociateVoicesFromBackgroundNoise"] = "dissociate_voices_from_background_noise";
+    MediaStepType["convertAudioToSubtitles"] = "convert_audio_to_subtitles";
+    MediaStepType["translateSubtitles"] = "translate_subtitles";
+    MediaStepType["cloneVoices"] = "clone_voices";
+    MediaStepType["translateVoices"] = "translate_voices";
+    MediaStepType["stretchVoices"] = "stretch_voices";
+    MediaStepType["detectFaces"] = "detect_faces";
+    MediaStepType["associateSpeakersToFaces"] = "associate_speakers_to_faces";
+    MediaStepType["syncLips"] = "sync_lips";
+    MediaStepType["upgradeQuality"] = "upgrade_quality";
+})(MediaStepType || (MediaStepType = {}));
+//# sourceMappingURL=media-step-type.enum.js.map
