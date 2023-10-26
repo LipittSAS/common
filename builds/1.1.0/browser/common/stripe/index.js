b1e3f58cf6b68a6a3ab8452fc33887c9409b4322
Loïc Combis <combis.loic@gmail.com>
2023-10-26 22:18:27 +0200

    Clean up v1.1.0

--- builds/1.1.0/browser/common/stripe/index.js
+++
@@ -1,4 +0,0 @@
-export * from './lib/stripe-subscription.interface';
-export * from './lib/enums/stripe-subscription-recurring.enum';
-export * from './lib/enums/stripe-subscription-status.enum';
-//# sourceMappingURL=index.js.map
