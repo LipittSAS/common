db952edc125397b4d267f3cd6abe1e0de8f58e10
Loïc Combis <combis.loic@gmail.com>
2023-10-28 11:38:22 +0200

    Build v1.2.0

---
+++ builds/1.2.0/browser/core/media/lib/media-step-type.enum.d.ts
@@ -0,0 +1,14 @@
+export declare enum MediaStepType {
+    splitAudioAndVideo = "split_audio_and_video",
+    dissociateVoicesFromBackgroundNoise = "dissociate_voices_from_background_noise",
+    convertAudioToSubtitles = "convert_audio_to_subtitles",
+    fixProperNames = "fix_proper_names",
+    translateSubtitles = "translate_subtitles",
+    cloneVoices = "clone_voices",
+    translateVoices = "translate_voices",
+    stretchVoices = "stretch_voices",
+    detectFaces = "detect_faces",
+    associateSpeakersToFaces = "associate_speakers_to_faces",
+    syncLips = "sync_lips",
+    upgradeQuality = "upgrade_quality"
+}
