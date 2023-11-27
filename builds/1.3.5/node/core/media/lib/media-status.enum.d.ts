768ded53b6575fdc0940b471a0ac572bd5031f14
Loïc Combis <combis.loic@gmail.com>
2023-11-27 20:14:25 +0100

    Build v1.3.5

---
+++ builds/1.3.5/node/core/media/lib/media-status.enum.d.ts
@@ -0,0 +1,8 @@
+export declare enum MediaStatus {
+    uploaded = "uploaded",
+    awaitingProcessing = "awaiting_process",
+    processing = "processing",
+    ready = "ready",
+    downloaded = "downloaded",
+    error = "error"
+}
