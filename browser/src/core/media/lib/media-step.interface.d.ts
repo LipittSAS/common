009d683e64c783cb008da380a9a98b24195438be
Loïc Combis <combis.loic@gmail.com>
2023-12-01 17:05:04 +0100

    Build v1.4.4

---
+++ browser/src/core/media/lib/media-step.interface.d.ts
@@ -0,0 +1,14 @@
+import { MediaStepStatus } from "./media-step-status.enum";
+import { MediaStepType } from "./media-step-type.enum";
+export interface MediaStep {
+    id: string;
+    mediaId: string;
+    order: number;
+    type: MediaStepType;
+    status: MediaStepStatus;
+    retries: number;
+    start: Date;
+    end: Date;
+    updatedAt: Date;
+    createdAt: Date;
+}
