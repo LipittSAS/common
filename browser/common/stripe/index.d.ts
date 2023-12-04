51156eccceaa708607a3195cf6aa894012256987
Loïc Combis <combis.loic@gmail.com>
2023-12-04 10:31:07 +0100

    Build v1.5.2

---
+++ browser/common/stripe/index.d.ts
@@ -0,0 +1,3 @@
+export * from './lib/stripe-subscription.interface';
+export * from './lib/enums/stripe-subscription-recurring.enum';
+export * from './lib/enums/stripe-subscription-status.enum';
---
+++ browser/core/auth/lib/auth.interface.js
@@ -0,0 +1,7 @@
+export class RegisterRequest {
+}
+export class VerifyRequest {
+}
+export class RefreshTokenRequest {
+}
+//# sourceMappingURL=auth.interface.js.map
