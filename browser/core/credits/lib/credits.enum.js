933fc3d4bb254b754a5cd43500feef261bad19dc
Loïc Combis <combis.loic@gmail.com>
2023-12-20 21:47:24 +0100

    Build v1.5.3

---
+++ browser/core/credits/lib/credits.enum.js
@@ -0,0 +1,7 @@
+export var CreditsStatus;
+(function (CreditsStatus) {
+    CreditsStatus["active"] = "active";
+    CreditsStatus["ended"] = "ended";
+    CreditsStatus["scheduled"] = "scheduled";
+})(CreditsStatus || (CreditsStatus = {}));
+//# sourceMappingURL=credits.enum.js.map
