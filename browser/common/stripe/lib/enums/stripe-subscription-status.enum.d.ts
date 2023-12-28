fe23f10c0619f1ccff25ff4233c3b125093e91a2
ZiyedB <zdiyez+githubjob@gmail.com>
2023-12-28 13:21:04 +0100

    Build v1.7.3

---
+++ browser/common/stripe/lib/enums/stripe-subscription-status.enum.d.ts
@@ -0,0 +1,9 @@
+export declare enum StripeSubscriptionStatus {
+    Trialing = "trialing",
+    Active = "active",
+    PastDue = "past_due",
+    Canceled = "canceled",
+    Unpaid = "unpaid",
+    Incomplete = "incomplete",
+    IncompleteExpired = "incomplete_expired"
+}
