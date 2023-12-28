4801762e08f8133946134348145e09456c413039
ZiyedB <zdiyez+githubjob@gmail.com>
2023-12-28 13:16:32 +0100

    Build v1.7.2

---
+++ browser/core/media/lib/media-status.enum.js
@@ -0,0 +1,10 @@
+export var MediaStatus;
+(function (MediaStatus) {
+    MediaStatus["uploaded"] = "uploaded";
+    MediaStatus["awaitingProcessing"] = "awaiting_process";
+    MediaStatus["processing"] = "processing";
+    MediaStatus["ready"] = "ready";
+    MediaStatus["downloaded"] = "downloaded";
+    MediaStatus["error"] = "error";
+})(MediaStatus || (MediaStatus = {}));
+//# sourceMappingURL=media-status.enum.js.map
