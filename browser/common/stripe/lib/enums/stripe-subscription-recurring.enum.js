077371137b28675266e04df75dcac29fc5626a58
ZiyedB <zdiyez+githubjob@gmail.com>
2023-12-28 13:16:40 +0100

    Clean up v1.7.2

--- browser/common/stripe/lib/enums/stripe-subscription-recurring.enum.js
+++
@@ -1,6 +0,0 @@
-export var StripeSubscriptionRecurring;
-(function (StripeSubscriptionRecurring) {
-    StripeSubscriptionRecurring["monthly"] = "month";
-    StripeSubscriptionRecurring["yearly"] = "year";
-})(StripeSubscriptionRecurring || (StripeSubscriptionRecurring = {}));
-//# sourceMappingURL=stripe-subscription-recurring.enum.js.map
