d16cdd78899943ab88cb4b6736fd8a2a2fa80eed
ZiyedB <zdiyez+githubjob@gmail.com>
2023-12-28 13:20:47 +0100

    fix: subscription make it optional

--- src/core/subscription/lib/subscription.interface.ts
+++ src/core/subscription/lib/subscription.interface.ts
@@ -4,11 +4,11 @@ import { User } from "../../user";
 export interface SubscriptionLipitt {
   id: string;
   plan: string;
-  subscriptionId: string;
-  subscriptionStatus: StripeSubscriptionStatus;
-  customerId: string;
-  productId: string
-  userId: string;
+  subscriptionId?: string;
+  subscriptionStatus?: StripeSubscriptionStatus;
+  customerId?: string;
+  productId?: string
+  userId?: string;
   user?: User;
   createdAt?: Date;
   updatedAt?: Date;
