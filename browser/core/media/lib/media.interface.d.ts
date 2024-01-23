5e3107c5197dafdaca876b11bf1f915846aff686
Loïc Combis <combis.loic@gmail.com>
2024-01-23 11:20:27 +0100

    Build v1.7.8

---
+++ browser/core/media/lib/media.interface.d.ts
@@ -0,0 +1,28 @@
+import { SupportedLanguages } from "../../../common";
+import { User } from "../../user";
+import { MediaStatus } from "./media-status.enum";
+import { MediaStep } from "./media-step.interface";
+export interface Media {
+    id: string;
+    name: string;
+    originalSize: number;
+    originalExt: string;
+    status: MediaStatus;
+    estimation: number;
+    to: SupportedLanguages;
+    start: Date;
+    end: Date;
+    ownerId: string;
+    owner?: User;
+    transcription?: any[];
+    steps: MediaStep[];
+    credits?: number;
+    duration?: number;
+    previewUrl?: string;
+    updatedAt: Date;
+    createdAt: Date;
+}
+export interface CreateMediaRequest {
+    name: string;
+    target: SupportedLanguages;
+}
