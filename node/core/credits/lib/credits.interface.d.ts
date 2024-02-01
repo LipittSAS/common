bef1540562f8959356b6fedb4a796ecf26291d1f
Loïc Combis <combis.loic@gmail.com>
2024-02-01 19:26:38 +0100

    Build v1.7.10

---
+++ node/core/credits/lib/credits.interface.d.ts
@@ -0,0 +1,17 @@
+import { User } from "../../user";
+import { CreditsStatus } from "./credits.enum";
+export interface Credits {
+    id: string;
+    plan: string;
+    credits?: number;
+    pendingCredits?: number;
+    usedCredits?: number;
+    status?: CreditsStatus;
+    start?: Date;
+    end?: Date;
+    customerId: string;
+    userId?: string;
+    user?: User;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
