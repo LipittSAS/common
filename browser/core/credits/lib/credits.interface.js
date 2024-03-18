8900b2914bd728c8f0757825f2c6105b3e2a3f1a
Loïc Combis <combis.loic@gmail.com>
2024-03-18 16:31:16 +0100

    Build v1.8.0

---
+++ browser/core/credits/lib/credits.interface.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=credits.interface.js.map
---
+++ browser/core/media/lib/media-step.interface.d.ts
@@ -0,0 +1,15 @@
+import { MediaStepStatus } from "./media-step-status.enum";
+import { MediaStepType } from "./media-step-type.enum";
+export interface MediaStep {
+    id: string;
+    mediaId: string;
+    order: number;
+    type: MediaStepType;
+    status: MediaStepStatus;
+    retries: number;
+    start: Date;
+    end: Date;
+    failure?: string;
+    updatedAt: Date;
+    createdAt: Date;
+}
