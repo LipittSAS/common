69c87bcfb3c20bf827e229e7e036280cf2afed17
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-05-27 11:04:06 +0200

    Build v1.9.7

---
+++ node/core/media/lib/media-step-type.enum.d.ts
@@ -0,0 +1,14 @@
+export declare enum MediaStepType {
+    splitAudioAndVideo = "split_audio_video",
+    dissociateVoicesFromBackgroundNoise = "dissociate_voices_from_background",
+    voicesAndTranscriptionsBySpeaker = "voices_and_transcriptions_by_speaker",
+    fixTranscriptions = "fix_transcription",
+    translateTranscriptions = "translate_transcription",
+    cloneVoices = "clone_voices",
+    generateVoices = "generate_voices",
+    assembleAudio = "assemble_audio",
+    associateSpeakersToFaces = "associate_speakers_to_faces",
+    syncLips = "lip_sync",
+    syncLabsLipSync = "sync_labs_lip_sync",
+    assembleMedia = "assemble_media"
+}
