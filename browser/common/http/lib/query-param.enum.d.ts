a682b1411fa1a517fe23f6c4da66018819af4eb6
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-05-27 11:04:26 +0200

    Clean up v1.9.7

--- browser/common/http/lib/query-param.enum.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum QueryParam {
-    code = "code",
-    redirectUrl = "redirect-url",
-    take = "take",
-    skip = "skip",
-    filters = "filters"
-}
--- node/core/user/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './user-role.enum';
-export * from './user-state.enum';
-export * from './user.interface';
