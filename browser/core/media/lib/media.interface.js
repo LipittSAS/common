3d3b1a2e14d9b169b7f94329e942c6ef930d55b3
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-05-28 13:20:45 +0200

    Clean up v1.10.3

--- browser/core/media/lib/media.interface.js
+++
@@ -1,6 +0,0 @@
-export var MediaFlowType;
-(function (MediaFlowType) {
-    MediaFlowType["translation"] = "translation";
-    MediaFlowType["translation_no_lipsync"] = "translation_no_lipsync";
-})(MediaFlowType || (MediaFlowType = {}));
-//# sourceMappingURL=media.interface.js.map
