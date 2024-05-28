c305a5c88084239235453f7fac013f1cf60adf52
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-05-28 13:20:35 +0200

    Build v1.10.3

---
+++ node/core/media/lib/media-status.enum.js
@@ -0,0 +1,23 @@
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.MediaStatus = void 0;
+    var MediaStatus;
+    (function (MediaStatus) {
+        MediaStatus["uploaded"] = "uploaded";
+        MediaStatus["awaitingProcessing"] = "awaiting_process";
+        MediaStatus["processing"] = "processing";
+        MediaStatus["ready"] = "ready";
+        MediaStatus["downloaded"] = "downloaded";
+        MediaStatus["error"] = "error";
+    })(MediaStatus || (exports.MediaStatus = MediaStatus = {}));
+});
+//# sourceMappingURL=media-status.enum.js.map
