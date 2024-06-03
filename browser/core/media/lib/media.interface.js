6b1f8e5930927127750ccbfd4bf9a24c3c8dab58
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-03 16:23:28 +0200

    Build v1.10.6

---
+++ browser/core/media/lib/media.interface.js
@@ -0,0 +1,6 @@
+export var MediaFlowType;
+(function (MediaFlowType) {
+    MediaFlowType["translation"] = "translation";
+    MediaFlowType["translation_no_lipsync"] = "translation_no_lipsync";
+})(MediaFlowType || (MediaFlowType = {}));
+//# sourceMappingURL=media.interface.js.map
