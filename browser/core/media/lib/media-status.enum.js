52d8afa3d7bbdc89e4c668f5d57486a647cd1955
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-04 14:07:24 +0200

    Build v1.10.7

---
+++ browser/core/media/lib/media-status.enum.js
@@ -0,0 +1,11 @@
+export var MediaStatus;
+(function (MediaStatus) {
+    MediaStatus["uploaded"] = "uploaded";
+    MediaStatus["awaitingProcessing"] = "awaiting_process";
+    MediaStatus["processing"] = "processing";
+    MediaStatus["ready"] = "ready";
+    MediaStatus["downloaded"] = "downloaded";
+    MediaStatus["stopped"] = "stopped";
+    MediaStatus["error"] = "error";
+})(MediaStatus || (MediaStatus = {}));
+//# sourceMappingURL=media-status.enum.js.map
