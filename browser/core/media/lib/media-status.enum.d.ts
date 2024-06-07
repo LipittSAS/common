aa61a3463c532ef1c2d6edb94c3ad9207ef4abf7
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-07 11:17:25 +0200

    Clean up v1.10.11

--- browser/core/media/lib/media-status.enum.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum MediaStatus {
-    uploaded = "uploaded",
-    awaitingProcessing = "awaiting_process",
-    processing = "processing",
-    ready = "ready",
-    downloaded = "downloaded",
-    stopped = "stopped",
-    error = "error"
-}
