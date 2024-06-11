f50a3eb1d3b08ae6f38ad37b1c06d6463a9db17b
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-11 16:38:14 +0200

    Build v1.10.12

---
+++ browser/core/media/lib/media.interface.js
@@ -0,0 +1,11 @@
+export var MediaFlowType;
+(function (MediaFlowType) {
+    MediaFlowType["translation"] = "translation";
+    MediaFlowType["translation_no_lipsync"] = "translation_no_lipsync";
+})(MediaFlowType || (MediaFlowType = {}));
+export var TextReplacementMode;
+(function (TextReplacementMode) {
+    TextReplacementMode["technicalTerms"] = "technical_terms";
+    TextReplacementMode["properNames"] = "proper_names";
+})(TextReplacementMode || (TextReplacementMode = {}));
+//# sourceMappingURL=media.interface.js.map
