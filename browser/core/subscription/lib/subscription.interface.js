bb33befd406efc3639e7f51a35bc33d54510b36a
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-12 11:16:18 +0200

    Clean up v1.10.13

--- browser/core/subscription/lib/subscription.interface.js
+++
@@ -1,7 +0,0 @@
-export var SubscriptionPlan;
-(function (SubscriptionPlan) {
-    SubscriptionPlan["premium"] = "premium";
-    SubscriptionPlan["master"] = "master";
-    SubscriptionPlan["entreprise"] = "entreprise";
-})(SubscriptionPlan || (SubscriptionPlan = {}));
-//# sourceMappingURL=subscription.interface.js.map
