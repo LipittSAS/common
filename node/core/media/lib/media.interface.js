99871a2b68c520b959956edd1c0abb0a9106fa91
Loïc Combis <combis.loic@gmail.com>
2024-06-14 11:09:31 +0200

    Build v1.10.14

---
+++ node/core/media/lib/media.interface.js
@@ -0,0 +1,24 @@
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.TextReplacementMode = exports.MediaFlowType = void 0;
+    var MediaFlowType;
+    (function (MediaFlowType) {
+        MediaFlowType["translation"] = "translation";
+        MediaFlowType["translation_no_lipsync"] = "translation_no_lipsync";
+    })(MediaFlowType || (exports.MediaFlowType = MediaFlowType = {}));
+    var TextReplacementMode;
+    (function (TextReplacementMode) {
+        TextReplacementMode["technicalTerms"] = "technical_terms";
+        TextReplacementMode["properNames"] = "proper_names";
+    })(TextReplacementMode || (exports.TextReplacementMode = TextReplacementMode = {}));
+});
+//# sourceMappingURL=media.interface.js.map
