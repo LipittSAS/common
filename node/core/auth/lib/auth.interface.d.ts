92a2dc361d03bb1f6d8044282192dae6aea3af37
Loïc Combis <combis.loic@gmail.com>
2024-06-27 11:02:14 +0200

    Build v1.10.21

---
+++ node/core/auth/lib/auth.interface.d.ts
@@ -0,0 +1,9 @@
+export declare abstract class RegisterRequest {
+    abstract email: string;
+    abstract locale: string;
+    abstract affiliate?: string;
+    abstract name?: string;
+}
+export interface OkResponse {
+    ok: boolean;
+}
