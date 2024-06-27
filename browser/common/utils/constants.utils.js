06545a03ab8a76eefb2a1517e89059341533f1a0
Loïc Combis <combis.loic@gmail.com>
2024-06-27 09:46:11 +0200

    Build v1.10.19

---
+++ browser/common/utils/constants.utils.js
@@ -0,0 +1,14 @@
+export class ConstantUtils {
+    // see for email max length https://www.rfc-editor.org/errata/eid1690
+    static emailMaxLength = 320;
+    static nameMaxLength = 64;
+    static identifierMaxLength = 64;
+    static urlMaxLength = 255;
+    static hashLength = 128;
+    static basicHashMaxLength = 64;
+    static localeMaxLength = 35;
+    static uuidLength = 36;
+    static version = 'v1';
+    static supportedMimeTypes = ['video/mp4', 'video/mpeg', 'video/quicktime', 'video/x-msvideo'];
+}
+//# sourceMappingURL=constants.utils.js.map
