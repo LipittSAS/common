1ea44ee17cf631928d36106ad2f3cc1f15dea7cb
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-06-28 15:07:00 +0200

    Build v1.10.22

---
+++ browser/core/subscription/lib/subscription.interface.js
@@ -0,0 +1,7 @@
+export var SubscriptionPlan;
+(function (SubscriptionPlan) {
+    SubscriptionPlan["premium"] = "premium";
+    SubscriptionPlan["master"] = "master";
+    SubscriptionPlan["entreprise"] = "enterprise";
+})(SubscriptionPlan || (SubscriptionPlan = {}));
+//# sourceMappingURL=subscription.interface.js.map
