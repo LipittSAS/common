1b630600da5cfc20da90103fc736143de3d4ccb9
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-07-05 12:26:45 +0200

    Build v1.10.26

---
+++ browser/core/media/lib/media.interface.js
@@ -0,0 +1,17 @@
+export var MediaFlowType;
+(function (MediaFlowType) {
+    MediaFlowType["translation"] = "translation";
+    MediaFlowType["translation_v2"] = "translation_v2";
+    MediaFlowType["translation_no_lipsync"] = "translation_no_lipsync";
+    MediaFlowType["translation_no_lipsync_v2"] = "translation_no_lipsync_v2";
+    MediaFlowType["translation_caption"] = "translation_caption";
+    MediaFlowType["translation_caption_no_lipsync"] = "translation_caption_no_lipsync";
+    MediaFlowType["translation_with_check"] = "translation_check";
+    MediaFlowType["translation_check_no_lipsync"] = "translation_check_no_lipsync";
+})(MediaFlowType || (MediaFlowType = {}));
+export var TextReplacementMode;
+(function (TextReplacementMode) {
+    TextReplacementMode["technicalTerms"] = "technical_terms";
+    TextReplacementMode["properNames"] = "proper_names";
+})(TextReplacementMode || (TextReplacementMode = {}));
+//# sourceMappingURL=media.interface.js.map
