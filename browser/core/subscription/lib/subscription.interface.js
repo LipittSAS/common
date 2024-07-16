fc9df2c2b1487a9952b08a27bf2bdb0ca3120e1d
Loïc Combis <combis.loic@gmail.com>
2024-07-16 11:23:59 +0200

    Clean up v1.11.1

--- browser/core/subscription/lib/subscription.interface.js
+++
@@ -1,7 +0,0 @@
-export var SubscriptionPlan;
-(function (SubscriptionPlan) {
-    SubscriptionPlan["premium"] = "premium";
-    SubscriptionPlan["master"] = "master";
-    SubscriptionPlan["entreprise"] = "enterprise";
-})(SubscriptionPlan || (SubscriptionPlan = {}));
-//# sourceMappingURL=subscription.interface.js.map
