d17fb15af9985f8d8a2ce368e71aaaa0179e9b8c
Loïc Combis <combis.loic@gmail.com>
2024-07-16 14:45:12 +0200

    Build v1.11.2

---
+++ browser/core/media/lib/media.interface.js
@@ -0,0 +1,10 @@
+export var MediaFlowType;
+(function (MediaFlowType) {
+    MediaFlowType["translation"] = "translation";
+})(MediaFlowType || (MediaFlowType = {}));
+export var TextReplacementMode;
+(function (TextReplacementMode) {
+    TextReplacementMode["technicalTerms"] = "technical_terms";
+    TextReplacementMode["properNames"] = "proper_names";
+})(TextReplacementMode || (TextReplacementMode = {}));
+//# sourceMappingURL=media.interface.js.map
