bc02a82b408702b015a32effdf7f495e258712f6
Cyp-Leg <cyprien@lipitt.com>
2024-07-26 15:28:33 +0200

    Build v1.11.10

---
+++ browser/core/media/lib/media-status.enum.d.ts
@@ -0,0 +1,10 @@
+export declare enum MediaStatus {
+    uploaded = "uploaded",
+    awaitingProcessing = "awaiting_process",
+    processing = "processing",
+    ready = "ready",
+    downloaded = "downloaded",
+    stopped = "stopped",
+    error = "error",
+    partiallyFailed = "partially_failed"
+}
