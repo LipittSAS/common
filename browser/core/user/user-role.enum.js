9891ecf0aef1fba11f8a55aa40d379dd71ad4d07
Cyp-Leg <cyprien@lipitt.com>
2024-07-26 15:28:45 +0200

    Clean up v1.11.10

--- browser/core/user/user-role.enum.js
+++
@@ -1,7 +0,0 @@
-export var UserRole;
-(function (UserRole) {
-    UserRole["user"] = "user";
-    UserRole["staff"] = "staff";
-    UserRole["admin"] = "admin";
-})(UserRole || (UserRole = {}));
-//# sourceMappingURL=user-role.enum.js.map
