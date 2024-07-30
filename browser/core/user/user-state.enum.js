0eaa2593b3ae2292efb55e5a758c615f02a733b2
Cyp-Leg <cyprien@lipitt.com>
2024-07-30 10:38:24 +0200

    Clean up v1.11.12

--- browser/core/user/user-state.enum.js
+++
@@ -1,6 +0,0 @@
-export var UserState;
-(function (UserState) {
-    UserState["active"] = "active";
-    UserState["inactive"] = "inactive";
-})(UserState || (UserState = {}));
-//# sourceMappingURL=user-state.enum.js.map
