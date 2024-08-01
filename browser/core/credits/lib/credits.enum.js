acea84925f3f3253e9074a7a4b122a16d89f3d62
Loïc Combis <combis.loic@gmail.com>
2024-08-01 08:44:18 +0200

    Build v1.11.16

---
+++ browser/core/credits/lib/credits.enum.js
@@ -0,0 +1,8 @@
+export var CreditsStatus;
+(function (CreditsStatus) {
+    CreditsStatus["active"] = "active";
+    CreditsStatus["ended"] = "ended";
+    CreditsStatus["paused"] = "paused";
+    CreditsStatus["deleted"] = "deleted";
+})(CreditsStatus || (CreditsStatus = {}));
+//# sourceMappingURL=credits.enum.js.map
---
+++ browser/core/subscription/lib/subscription.interface.js
@@ -0,0 +1,8 @@
+export var SubscriptionPlan;
+(function (SubscriptionPlan) {
+    SubscriptionPlan["premium"] = "premium";
+    SubscriptionPlan["master"] = "master";
+    SubscriptionPlan["entreprise"] = "enterprise";
+    SubscriptionPlan["custom"] = "custom";
+})(SubscriptionPlan || (SubscriptionPlan = {}));
+//# sourceMappingURL=subscription.interface.js.map
