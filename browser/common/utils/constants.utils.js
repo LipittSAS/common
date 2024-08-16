5a9323eff5e44c95cc8683f1a78c4883f5326dc7
ahmadou23 <166594963+ahmadou23@users.noreply.github.com>
2024-08-16 16:47:19 +0200

    Build v1.11.23

---
+++ browser/common/utils/constants.utils.js
@@ -0,0 +1,15 @@
+export class ConstantUtils {
+    // see for email max length https://www.rfc-editor.org/errata/eid1690
+    static emailMaxLength = 320;
+    static nameMaxLength = 64;
+    static identifierMaxLength = 64;
+    static urlMaxLength = 255;
+    static hashLength = 128;
+    static basicHashMaxLength = 64;
+    static localeMaxLength = 35;
+    static uuidLength = 36;
+    static version = 'v1';
+    static supportedVideoMimeTypes = ['video/mp4', 'video/mpeg', 'video/quicktime', 'video/x-msvideo'];
+    static supportedAudioMimeTypes = ['audio/mp3', 'audio/flac', 'audio/wav'];
+}
+//# sourceMappingURL=constants.utils.js.map
