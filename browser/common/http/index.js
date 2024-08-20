1413114d9ed058fb42d93fef858f7d0c661e8474
Cyp-Leg <cyprien@lipitt.com>
2024-08-20 09:52:40 +0200

    Clean up v1.11.24

--- browser/common/http/index.js
+++
@@ -1,4 +0,0 @@
-export * from './lib/query-param.enum';
-export * from './lib/list-response.interface';
-export * from './lib/metadata.interface';
-//# sourceMappingURL=index.js.map
--- browser/common/http/lib/metadata.interface.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Metadata<T> {
-    take: number;
-    skip: number;
-    total: number;
-    filters?: Record<keyof T, unknown>;
-}
