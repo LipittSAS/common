0b32506f59edcbf0e3bc548c451bb85e9722e296
Cyp-Leg <cyprien@lipitt.com>
2024-08-30 17:11:17 +0200

    Clean up v1.11.29

--- browser/common/http/lib/query-param.enum.js
+++
@@ -1,9 +0,0 @@
-export var QueryParam;
-(function (QueryParam) {
-    QueryParam["code"] = "code";
-    QueryParam["redirectUrl"] = "redirect-url";
-    QueryParam["take"] = "take";
-    QueryParam["skip"] = "skip";
-    QueryParam["filters"] = "filters";
-})(QueryParam || (QueryParam = {}));
-//# sourceMappingURL=query-param.enum.js.map
