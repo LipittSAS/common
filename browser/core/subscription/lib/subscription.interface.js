15ecac970d2e83054f0326974c5c74f8c7231f4f
ahmadou23 <ahmadou@lipitt.com>
2024-09-12 17:51:25 +0200

    Clean up v1.11.36

--- browser/core/subscription/lib/subscription.interface.js
+++
@@ -1,8 +0,0 @@
-export var SubscriptionPlan;
-(function (SubscriptionPlan) {
-    SubscriptionPlan["premium"] = "premium";
-    SubscriptionPlan["master"] = "master";
-    SubscriptionPlan["entreprise"] = "enterprise";
-    SubscriptionPlan["custom"] = "custom";
-})(SubscriptionPlan || (SubscriptionPlan = {}));
-//# sourceMappingURL=subscription.interface.js.map
