1afef13c90fcb20f2c89896de24dbf5960902a7b
Cyp-Leg <cyprien@lipitt.com>
2024-09-26 17:36:47 +0200

    Build v1.11.41

---
+++ browser/core/media/lib/media.interface.js
@@ -0,0 +1,20 @@
+export var MediaFlowType;
+(function (MediaFlowType) {
+    MediaFlowType["translation"] = "translation";
+    MediaFlowType["voiceIsolation"] = "voice_isolation";
+    MediaFlowType["getSubtitles"] = "get_subtitles";
+    MediaFlowType["subtitlesTranslation"] = "subtitlesTranslation";
+    MediaFlowType["removeSubtitles"] = "remove_subtitles";
+})(MediaFlowType || (MediaFlowType = {}));
+export var TextReplacementMode;
+(function (TextReplacementMode) {
+    TextReplacementMode["technicalTerms"] = "technical_terms";
+    TextReplacementMode["properNames"] = "proper_names";
+})(TextReplacementMode || (TextReplacementMode = {}));
+export var MediaType;
+(function (MediaType) {
+    MediaType["audio"] = "audio";
+    MediaType["video"] = "video";
+    MediaType["text"] = "text";
+})(MediaType || (MediaType = {}));
+//# sourceMappingURL=media.interface.js.map
