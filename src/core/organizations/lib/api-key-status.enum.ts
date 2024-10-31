1e9b6b1a6f4520399c3066ccc5394f05bbe7ceb2
Cyp-Leg <cyprien@lipitt.com>
2024-10-31 15:24:42 +0100

    Add ApiKey interface

---
+++ src/core/organizations/lib/api-key-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ApiKeyStatus {
+    active = 'active',
+    disabled = 'disabled'
+  };
---
+++ src/core/organizations/lib/api-key.interface.ts
@@ -0,0 +1,11 @@
+import { ApiKeyStatus } from "./api-key-status.enum";
+
+export type ApiKey = {
+    id: string;
+    name: string;
+    firstChars: string;
+    lastChars: string;
+    hash: string;
+    createdAt: string;
+    status: ApiKeyStatus;
+  };
