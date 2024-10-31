85263baf178329ac36ca6de1140be52ac3818e96
Cyp-Leg <cyprien@lipitt.com>
2024-10-31 15:24:53 +0100

    Build v1.11.48

---
+++ browser/core/organizations/lib/api-key-status.enum.js
@@ -0,0 +1,7 @@
+export var ApiKeyStatus;
+(function (ApiKeyStatus) {
+    ApiKeyStatus["active"] = "active";
+    ApiKeyStatus["disabled"] = "disabled";
+})(ApiKeyStatus || (ApiKeyStatus = {}));
+;
+//# sourceMappingURL=api-key-status.enum.js.map
