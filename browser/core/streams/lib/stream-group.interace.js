86cb76ac2097785952706f6135d83653c8d7f8ce
Cyp-Leg <cyprien@lipitt.com>
2025-04-01 15:24:33 +0200

    Clean up v1.11.60

--- browser/core/streams/lib/stream-group.interace.js
+++
@@ -1,7 +0,0 @@
-;
-export var StreamGroupType;
-(function (StreamGroupType) {
-    StreamGroupType["DEFAULT"] = "default";
-    StreamGroupType["PLAYGROUND"] = "playground";
-})(StreamGroupType || (StreamGroupType = {}));
-//# sourceMappingURL=stream-group.interace.js.map
--- browser/core/streams/lib/stream-state.enum.js
+++
@@ -1,8 +0,0 @@
-export var StreamState;
-(function (StreamState) {
-    StreamState["active"] = "active";
-    StreamState["paused"] = "paused";
-    StreamState["stopped"] = "stopped";
-})(StreamState || (StreamState = {}));
-;
-//# sourceMappingURL=stream-state.enum.js.map
